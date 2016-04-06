28767a011eff30d24292cfbaaf1784b3e84c7671
Gustavo Guichard <gustavoguichard@gmail.com>
2016-04-06 20:06:42 -0300

    Add helper to get Int from inputs like range

--- helpers/Utils.elm
+++ helpers/Utils.elm
@@ -1,8 +1,30 @@
 module Helpers.Utils (..) where
 
+import Html exposing (Attribute)
 import Html.Events exposing (on, targetValue)
 import Signal
+import String
 
 
+onInput : Signal.Address a -> (String -> a) -> Attribute
 onInput address action =
   on "input" targetValue (Signal.message address << action)
+
+
+onIntChange : Signal.Address a -> (Int -> a) -> Attribute
+onIntChange address action =
+  on "input" targetValue (Signal.message address << action << convertToInt)
+
+
+convertToInt : String -> Int
+convertToInt string =
+  let
+    int' =
+      String.toInt string
+  in
+    case int' of
+      Ok n ->
+        n
+
+      _ ->
+        0
