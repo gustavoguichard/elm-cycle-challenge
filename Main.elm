7fd17505943e09dc9896a660d8487493916adb0c
Gustavo Guichard <gustavoguichard@gmail.com>
2016-04-06 17:21:52 -0300

    First example, Clock

--- Main.elm
+++ Main.elm
@@ -1,10 +1,12 @@
 module Main (..) where
 
-import Html exposing (..)
-import Signal
-import StartApp exposing (start)
-import Time exposing (every, second)
 import Effects
+import Html exposing (..)
+import Html.Attributes exposing (..)
+import Html.Events exposing (..)
+import Signal
+import StartApp
+import Time exposing (every, second)
 
 
 -- MODEL
@@ -26,11 +28,20 @@ model =
 type Action
   = NoOp
   | Tick
+  | ResetClock
 
 
 update : Action -> Model -> ( Model, Effects.Effects a )
 update action model =
-  ( model, Effects.none )
+  case action of
+    Tick ->
+      ( { model | time = model.time + 1 }, Effects.none )
+
+    ResetClock ->
+      ( { model | time = 0 }, Effects.none )
+
+    NoOp ->
+      ( model, Effects.none )
 
 
 
@@ -39,7 +50,11 @@ update action model =
 
 view : Signal.Address Action -> Model -> Html
 view address model =
-  div [] []
+  h1
+    [ style [ ( "background", "royalblue" ) ]
+    , onMouseEnter address ResetClock
+    ]
+    [ span [] [ text <| "seconds elapsed: " ++ toString model.time ] ]
 
 
 
@@ -48,7 +63,7 @@ view address model =
 
 app : StartApp.App Model
 app =
-  start
+  StartApp.start
     { view = view
     , update = update
     , init = ( model, Effects.none )
